3e2821287e3a6a2709afe36f0e551c9e3eda02ff
Ivan Diaz <39617173+Wlgen@users.noreply.github.com>
2019-04-05 16:26:45 +0200

    Add files via upload

---
+++ customi3lock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+IMAGE=/tmp/lockscreen.png
+TEXT=/tmp/locktext.png
+ICON=/home/ivan/Imágenes/iconfinder_Website_lock_icon_2541670.png
+scrot $IMAGE
+convert $IMAGE -scale 10% -scale 1000% -fill black -colorize 25% $IMAGE
+[ -f $TEXT ] || {
+    convert -size 3000x60 xc:white -font Liberation-Sans -pointsize 26 -fill black -gravity center -annotate +0+0 'Type password to unlock' $TEXT;
+    convert $TEXT -alpha set -channel A -evaluate set 50% $TEXT;
+}
+convert $IMAGE $TEXT -gravity center -geometry +0+200 -composite $IMAGE
+convert $IMAGE $ICON -gravity center -composite -matte $IMAGE
+i3lock -u -i $IMAGE
